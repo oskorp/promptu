3aae94110a0ec74208688192bde6e32430897c0d
oskorp <omkarkhandalkar846@gmail.com>
2023-07-22 00:03:40 +0530

    added auth code and provdier

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
@@ -9,7 +10,7 @@ const Home = () => {
       <p className="desc text-center">
         Propmtopia is an open source AI propmpting tool for modern world to discover,create and share creative propmts
       </p>
-
+      
     </section>
   );
 };
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,51 +1,99 @@
 "use client";
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { useState,useEffect } from 'react'
-import { useSession, signIn, signOut } from "next-auth/react"
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useSession, signIn, signOut, getProviders } from "next-auth/react";
 const Nav = () => {
-  const isUserIsLoggedIn=true;
+  const isUserIsLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
+
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-    <Link href="/" className="flex gap-2 flex-center">
-        <Image 
-        src="/assets/images/logo.svg"
-        alt="Promptopia Logo"
-        width={30} 
-        height={30}
-        className="object-contain"
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
         />
-    </Link>
-    <div className="sm:flex hidden">
+      </Link>
+      <div className="sm:flex hidden">
         {isUserIsLoggedIn ? (
-            <div className='flex gap-3 md:gap-5'>
-                <Link href="/create-prompt" className="black_btn">
-                    Create Post
-                </Link>
-                <button 
-                type="button" 
-                onClick={signOut}
-                className="outline_btn">
-                Sign Out
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                className="rounded-full"
+                alt="profile"
+                width={37}
+                height={37}
+              ></Image>
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((providers) => (
+                <button
+                  type="button"
+                  key={providers.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
                 </button>
+              ))}
+          </>
+        )}
+      </div>
 
-                <Link href="/profile">
-                    <Image
-                    src="/assets/images/logo.svg"
-                    className="rounded-full"
-                    alt="profile"
-                    width={37}
-                    height={37}>
-
-                    </Image>
-                </Link>
-            </div>
-            ):(<></>)}
-    </div>
+      {/* mobile navigation */}
+      <div className="sm:hidden flex relative">
+        {isUserIsLoggedIn ? (
+          <div>
+            <Image
+              src="/assets/images/logo.svg"
+              className="rounded-full"
+              alt="profile"
+              width={37}
+              height={37}
+              onClick={() => {}}
+            ></Image>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((providers) => (
+                <button
+                  type="button"
+                  key={providers.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
-    
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
